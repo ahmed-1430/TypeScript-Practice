fa3d232d23887addc98387437fdd058149b6c2a1
AHMED <ahmedtexting@gmail.com>
2026-05-03 11:00:20 +0600

    non primitive types and tuple

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,16 @@
+// array, object
+
+let bazarList = ['eggs', 'milk', 'sugar'];
+
+// bazarList.push(123);     // number not assignable to a string
+
+let mixedArray : (string | number)[] = ['mango', 3, 'apple', 2, 'milk', 4, 'sugar']
+// mixedArray.push(true);  // not assignable cause array accept string and number only
+
+// ts => tuple
+
+let coordinates : [number, number] = [30, 40];
+
+let couple : [string,  string] = ["husband", "wife"];
+
+let nameRoll : [string, number] = ["Ahmed", 19]
