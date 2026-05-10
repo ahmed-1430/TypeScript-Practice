928e602a5ca4fb4179a2776126a7504415e6cb77
AHMED <ahmedtexting@gmail.com>
2026-05-11 01:25:33 +0600

    generic with functions

--- src/genericFunction.ts
+++ src/genericFunction.ts
@@ -45,4 +45,32 @@ const createArrayTuppleWithGeneric = <X, Y>(param1: X, param2: Y) => {
 
 const result4 = createArrayTuppleWithGeneric('ahmed', true)
 
-const result5 = createArrayTuppleWithGeneric('Ahmed', {id: 123, name: 'Ahmed'})
+const result5 = createArrayTuppleWithGeneric('Ahmed', { id: 123, name: 'Ahmed' })
+
+
+// add course for student function 
+
+const addStudentToCourse = <T>(studentInfo: T) => {
+    return {
+        course: "Next Level",
+        ...studentInfo
+    };
+}
+
+const student1 = {
+    id: 426,
+    name: "xyz",
+    isPen: true
+};
+
+const student2 = {
+    id: 845,
+    name: 'abc',
+    isMarried: false
+}
+
+const result1 = addStudentToCourse(student1);
+console.log({result1});
+
+const result2 = addStudentToCourse(student2);
+console.log({result2});
