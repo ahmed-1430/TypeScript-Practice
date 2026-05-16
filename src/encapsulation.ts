ff155e7ba131e7955bae7b678d707ab80cdd92db
AHMED <ahmedtexting@gmail.com>
2026-05-17 04:20:50 +0600

    encapsulation => 4th pillar of OOP

---
+++ src/encapsulation.ts
@@ -0,0 +1,25 @@
+// Encapsulation 
+
+
+
+class BankAccount {
+    userId: number;
+    userName: string;
+    private userBalance: number;           
+    constructor(userId: number, userName: string, userBalance: number) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userBalance = userBalance;
+    }
+    addBalance(balance: number){
+        this.userBalance = this.userBalance + balance
+    }
+
+}
+
+const AhmedBankAccount = new BankAccount(152, 'Ahmed', 50);
+
+AhmedBankAccount.addBalance(50);
+AhmedBankAccount.addBalance(110);
+
+console.log(AhmedBankAccount);
